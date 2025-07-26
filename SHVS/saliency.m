4e3b6bf6b8a14ab273e5bd2d22bf1d202840d689
Yichuang Luo <131516035+CHUANGL8346@users.noreply.github.com>
2025-07-26 16:50:42 +0800

    Delete SHVS directory

--- SHVS/saliency.m
+++
@@ -1,9 +0,0 @@
-function y=saliency(x,r)
-
-[hei, wid] = size(x);
-N = boxfilter(ones(hei, wid), r);
-mean_xx = boxfilter(x.^2,r);
-mean_x = boxfilter(x,r);
-y = abs(mean_xx-2*x.*mean_x+(x.^2).*N);
-
-end
