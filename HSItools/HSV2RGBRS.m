d20a478a8951cd9cd8007151c4ddbcce7c4d9a6a
Yichuang Luo <131516035+CHUANGL8346@users.noreply.github.com>
2025-07-26 16:49:59 +0800

    Delete HSItools directory

--- HSItools/HSV2RGBRS.m
+++
@@ -1,7 +0,0 @@
-function r = HSV2RGBRS(img)
-%HSV2RGBRS 此处显示有关此函数的摘要
-%   此处显示详细说明
-img1 = hsv2rgb(img);
-r = cat(3, img1(:, :, 3), img1(:, :, 2), img1(:, :, 1));
-end
-
--- HSItools/RGB2HSIRS.m
+++
@@ -1,7 +0,0 @@
-function r = RGB2HSIRS(img)
-%RGB2HSIRS 此处显示有关此函数的摘要
-%   此处显示详细说明
-img1 = cat(3, img(:, :, 3), img(:, :, 2), img(:, :, 1));
-r = rgb2hsi(img1);
-end
-
